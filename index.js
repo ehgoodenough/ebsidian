61d87d124267734654faa8c43ff993705df3e370
Andrew McPherson <ehgoodenough@gmail.com>
2019-01-23 15:39:35 -0800

    Added methods for pubsub integration.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const lodash = require("lodash")
 const jwt = require("jsonwebtoken")
-const fetchquest = require("fetchquest")
-const secretsauce = require("secretsauce")
 
 const Urrl = require("urrl")
+const FetchQuest = require("fetchquest")
 
 const TwitchEBS = module.exports = {
     "extension": {
@@ -12,6 +11,89 @@ const TwitchEBS = module.exports = {
         "version": undefined,
     }
 }
+
+const EXPIRATION_TIME = 1000
+
+const TWITCH_EXT_CHAT_URL = new Urrl("https://api.twitch.tv/extensions/{clientId}/{extensionVersion}/channels/{channelId}/chat")
+const TWITCH_EXT_LIVE_CHANNELS_URL = new Urrl("https://api.twitch.tv/extensions/{clientId}/live_activated_channels")
+const TWITCH_EXT_PUBSUB_URL = new Urrl("https://api.twitch.tv/extensions/message/{channelId}")
+
+TwitchEBS.generateToken = function(payload) {
+    payload.exp = payload.exp || Date.now() + EXPIRATION_TIME
+    payload.role = payload.role || "external"
+
+    return jwt.sign(payload, Buffer.from(TwitchEBS.extension.secret, "base64"))
+}
+
+TwitchEBS.sendChatMessage = async function chat(channelId, text) {
+    channelId = channelId + ""
+
+    const token = TwitchEBS.generateToken({
+        "role": "broadcaster",
+        "user_id": channelId,
+        "channel_id": channelId,
+    })
+
+    return await new FetchQuest({
+        "url": TWITCH_EXT_CHAT_URL({
+            "clientId": TwitchEBS.extension.id,
+            "extensionVersion": TwitchEBS.extension.version,
+            "channelId": channelId,
+        }),
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "Client-ID": TwitchEBS.extension.id,
+            "Authorization": `Bearer ${token}`,
+        },
+        "body": {"text": text},
+    })
+}
+
+TwitchEBS.sendPubsubMessage = async function(channelId, target, message) {
+    channelId = channelId + ""
+
+    const token = TwitchEBS.generateToken({
+        "role": "external",
+        "user_id": channelId,
+        "channel_id": channelId,
+        "pubsub_perms": {
+            "send": [target]
+        }
+    })
+
+    return await new FetchQuest({
+        "method": "POST",
+        "url": TWITCH_EXT_PUBSUB_URL({
+            "channelId": channelId || "all"
+        }),
+        "headers": {
+            "Content-Type": "application/json",
+            "Client-ID": TwitchEBS.extension.id,
+            "Authorization": `Bearer ${token}`,
+        },
+        "body": {
+            "targets": [target],
+            "content_type": "application/json",
+            "message": JSON.stringify(message),
+        }
+    })
+}
+
+TwitchEBS.broadcastPubsubMessage = async function(channelId, message) {
+    TwitchEBS.sendPubsubMessage(channelId, "broadcast", message)
+}
+
+TwitchEBS.retrieveLiveChannels = async function() {
+    let response = await fetchquest({
+        "url": TWITCH_EXT_LIVE_CHANNELS_URL({"clientId": TwitchEBS.extension.id}),
+        "headers": {"Client-ID": TwitchEBS.extension.id}
+    })
+
+    return response.channels
+}
+
+
 
 // , {"expiresIn": "24h"}
 // const DEFAULT_PAYLOAD = {
@@ -42,48 +124,3 @@ const TwitchEBS = module.exports = {
 //     "role": "broadcaster",
 //     "iat": Math.floor(Date.now() / 1000) - 60
 // }
-
-const EXPIRATION_TIME = 1000
-
-const TWITCH_EXT_CHAT_URL = new Urrl("https://api.twitch.tv/extensions/{clientId}/{extensionVersion}/channels/{channelId}/chat")
-const TWITCH_EXT_LIVE_CHANNELS_URL = new Urrl("https://api.twitch.tv/extensions/{clientId}/live_activated_channels")
-
-module.exports.sendChatMessage = async function chat(channelId, text) {
-    channelId = channelId + ""
-
-    let url = TWITCH_EXT_CHAT_URL({
-        "clientId": TwitchEBS.extension.id,
-        "extensionVersion": TwitchEBS.extension.version,
-        "channelId": channelId,
-    })
-
-    let user = {
-        "role": "broadcaster",
-        "user_id": channelId,
-        "exp": Date.now() + EXPIRATION_TIME,
-        "channel_id": channelId,
-    }
-
-    let secret = Buffer.from(TwitchEBS.extension.secret, "base64")
-    let authorization = jwt.sign(user, secret)
-
-    return await fetchquest({
-        "url": url,
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json",
-            "Client-ID": TwitchEBS.extension.id,
-            "Authorization": `Bearer ${authorization}`,
-        },
-        "body": {"text": text},
-    })
-}
-
-module.exports.retrieveLiveChannels = async function() {
-    let response = await fetchquest({
-        "url": TWITCH_EXT_LIVE_CHANNELS_URL({"clientId": TwitchEBS.extension.id}),
-        "headers": {"Client-ID": TwitchEBS.extension.id}
-    })
-
-    return response.channels
-}
--- test.js
+++ test.js
@@ -1,13 +1,19 @@
 const TwitchEBS = require(".")
+const Secretsauce = require("secretsauce")
 
 async function test() {
     TwitchEBS.extension.id = "ub1t6tr0utja1bvqu661bt3o5w5tr8"
-    // TwitchEBS.extension.secret = await secretsauce.get("dex-twitchext-secret")
-    // TwitchEBS.extension.version = "0.0.1"
+    TwitchEBS.extension.secret = await Secretsauce.get("dex-twitchext-secret")
+    TwitchEBS.extension.version = "0.0.1"
+
+    // TwitchEBS.broadcastPubsubMessage("88171886", {
+    //     "greetings": "Hello World!!"
+    // })
+    // TwitchEBS.sendChatMessage("88171886", "Hello World!!")
 }
 
 test().then((response) => {
-    console.log(response || "Done!")
+    console.log(response || "!!!")
 }).catch((error) => {
     console.log(error)
 })
